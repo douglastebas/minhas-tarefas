5f3109d34d2cbe0a91561332e843603a6bcc0875
Douglas Tebas <dougtebas@gmail.com>
2023-04-24 17:26:33 -0300

    Create constant value for colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color(0xFF5F52EE);
+
+const Color primaryBlack = Color(0xFF3A3A3A);
+const Color primaryGrey = Color(0xFF717171);
+const Color tdLightGrey = Color.fromARGB(255, 211, 211, 211);
+const Color primaryBGColor = Color(0xFFEEEFF5);
+
+// LG Logo colors
+const Color darkRed = Color(0xFF8B2A1F);
+const Color brightRed = Color(0xFF8B2A1F);
+const Color darkOrange = Color(0xFFC94229);
+const Color orange = Color(0xFFF68000);
+const Color darkyellow = Color(0xFFFBAE17);
+const Color yellow = Color(0xFFFED213);
+const Color darkblue = Color(0xFF3C60AC);
+const Color blue = Color(0xFF3C60AC);
+const Color brightBlue = Color(0xFF369AD5);
